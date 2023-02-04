644cfd4392af02c7530f608599b142a09a8d7406
harmgehog <polstia@gmail.com>
2023-02-04 15:36:20 +0300

    enumerable range

--- task52/Program.cs
+++ task52/Program.cs
@@ -1,25 +1,11 @@
-﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+﻿// Задача 52. Задайте матрицу из целых чисел. Найдите среднее арифметическое элементов 
 // в каждом столбце. Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-int[,] matrix; int n, m, sum;
-void ShowMTX()
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-            Console.Write("{0,4}", matrix[i, j]);
-        Console.WriteLine();
-    }
-}
-void GetMTX()
-{
-    for (int i = 0; i < n; i++) 
-        for (int j = 0; j < m; j++) 
-            matrix[i, j] = new Random().Next(1, 99);
-}
+int n, m, sum; int[][] matrix; // двухступенчатый вместо двумерного
+
 void ShowAvrgCols()
 {
     Console.Write("Среднее каждого столбца: ");
@@ -27,16 +13,28 @@ void ShowAvrgCols()
     {
         sum = 0;
         for (int j = 0; j < n; j++)
-            sum += matrix[j, i];
+            sum += matrix[j][i];
         Console.Write("{0,6:0.00}", (double)sum / m);
-        // Console.Write(Math.Round(Convert.ToDouble(sum) / m, 2) + " ");
     }
+}
+void ShowMTX()
+{
+    foreach (int[] item in matrix)
+    {
+        for (int j = 0; j < item.Length; j++)
+            Console.Write("{0,4}", item[j]);
+        Console.WriteLine();
+    }
+}
+void GetMTX()
+{
+    matrix = Enumerable.Range(0, n).Select(r =>
+        Enumerable.Range(0, m).Select(c => new Random().Next(1, 99)).ToArray()).ToArray();
 }
 void Main()
 {
     n = 6; // new Random().Next(3, 10); // int.Parse(Console.ReadLine()!)
     m = 6; // new Random().Next(3, 10); // int.Parse(Console.ReadLine()!)
-    matrix = new int[n, m];
     GetMTX(); ShowMTX(); ShowAvrgCols();
 }
 Main();
